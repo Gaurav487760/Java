6257c84ea443afbb8a67343c6fc2337ae4433d09
Gaurav487760 <Shivalegaurav487@gmail.com>
2025-08-06 13:42:07 +0530

    Add files via upload

---
+++ PracticalAssign01/SurfacedCylin.java
@@ -0,0 +1,13 @@
+package PracticalAssign01;
+
+public class SurfacedCylin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    double pi=3.14;
+    double r=3.5;
+    double h=2.7;
+    double Area=2*pi*r*h;
+    System.out.println("Surfaced Area of Cylinder"+Area);
+	}
+}
