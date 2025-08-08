7763ec053eddf8a7ae531d54e417bbb734514706
Gaurav <Shivalegaurav487@gmail.com>
2025-08-08 12:53:05 +0530

    Add files via upload

---
+++ PracticalAssign01/one2Dime.java
@@ -0,0 +1,10 @@
+package PracticalAssign01;
+import java.util.*;
+public class one2Dime {
+	public static void main(String[] args){
+	     Integer array[] = {40,55,63,17,22,68,89,97,89};
+	     Arrays.sort(array, Collections.reverseOrder());
+	     System.out.println(Arrays.toString(array));
+	   }
+	}
+
