a1e8e9979c99f80823b0c5c71f13f26cc2465931
Gaurav <Shivalegaurav487@gmail.com>
2025-08-08 13:11:25 +0530

    Add files via upload

---
+++ PracticalAssign01/Series2.java
@@ -0,0 +1,11 @@
+package PracticalAssign01;
+
+public class Series2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Numbers from 1 to 100:");
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i); 
+        }
+	}
+}
