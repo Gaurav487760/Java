d9264cd938a9baa822a0aafdb9d7c7d6445885e7
Gaurav <Shivalegaurav487@gmail.com>
2025-08-11 12:26:52 +0530

    Delete PracticalAssign01 directory

--- PracticalAssign01/1D.java
+++
@@ -1,9 +0,0 @@
-public class 1D {
-  public static void main(String[] args) {
-  int[] numbers = {10, 20, 30, 40, 50};
-  System.out.println("Elements of the 1D array:");
-for (int i = 0; i < numbers.length; i++) {
-  System.out.println("Element at index " + i + ": " + numbers[i]);
-        }
-    }
-}
--- PracticalAssign01/Descen.java
+++
@@ -1,9 +0,0 @@
-package PracticalAssign01;
-import java.util.*;
-public class Descen {
-	   public static void main(String[] args){
-	     Integer array[] = { 1, 2, 7, 16, 5,6 };
-	     Arrays.sort(array, Collections.reverseOrder());
-	     System.out.println(Arrays.toString(array));
-	   }
-	}
